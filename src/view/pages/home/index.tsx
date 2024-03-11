16e130b261ee129ce58dd4c5b8b1392b4c66b888
Danillo Meira <danillo_meira10@hotmail.com>
2024-03-11 17:55:40 -0300

    Alinhar textos no componente Home

--- src/view/pages/home/index.tsx
+++ src/view/pages/home/index.tsx
@@ -1,9 +1,9 @@
-
+import style from "./Home.module.css"
 const Home = () =>{
     return(
-        <>
+        <div className={style.homeContainer}>
         Home
-        </>
+        </div>
     )
     }
     export default Home
